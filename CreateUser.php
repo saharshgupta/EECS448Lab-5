f3f7f54d6ec1c5c0ca5552b5008a026ffe438ef2
Saharsh Gupta <s510g881@ku.edu>
2019-03-22 17:24:58 -0500

    Exercise 2 completed.

---
+++ CreateUser.php
@@ -0,0 +1,19 @@
+<?php
+$mysqli = new mysqli("mysql.eecs.ku.edu", "s510g881", "Wai9xei7", "s510g881");
+if($mysqli->connect_errno){
+	echo "Connection failed: " . $mysqli->connect_errno;
+	exit();
+}
+$user = $_POST["user_id"];
+$query = "INSERT INTO Users (user_id) VALUES('" . $user . "')";
+if($result = $mysqli->query($query)){
+	while ($row = $result->fetch_assoc()){
+		echo "Added User: $row[user_id]";
+	}
+	$result->free();
+}
+else{
+	echo "Addition failed, user already exists";
+}
+$mysqli->close();
+?>
